16bbd0978d7b5bd02bcf3be9bf0feb595480ce5f
matthewd28 <matthewpalmerdavis@gmail.com>
2024-08-07 10:14:38 -0600

    Create calculator UI

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const operators = document.querySelectorAll(".operator")
+const operands = document.querySelectorAll(".operands")
+const clear = document.querySelector("#clear")
+const percent = document.querySelector("#percent")
+
+let val = 0
+
+
